bfca4b12c6435a02f662e8da187817b332b9ace7
Kayla Vangel <Kayla.Vangel@comcast.net>
2024-01-21 14:21:14 -0500

    fucking work please

--- pages/_app.js
+++ pages/_app.js
@@ -2,18 +2,14 @@ import "bootstrap/dist/css/bootstrap.css";
 import '../styles/globals.css';
 import '../styles/index.css';
 import Layout from "./components/layout";
-import { useSSR } from '@nextui-org/react'
 
 
 function MyApp ({ Component, pageProps }) {
-  const { isBrowser } = useSSR();
   return (
-    isBrowser && (
     <Layout>
       <Component {...pageProps} />
     </Layout>
     )
-  )
 }
 
 export default MyApp
