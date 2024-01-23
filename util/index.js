bb23ae137ecb97c0d4492dbbb8ac498144878b05
James Morton <james_morton@brown.edu>
2024-01-22 19:34:54 -0500

    please. why.

--- util/index.js
+++ util/index.js
@@ -4,7 +4,9 @@ import path from "path";
 export async function getData() {
     // Call an external API endpoint to get posts
     const res = await fs.readFile(path.resolve(process.cwd(), "./data/data.json"), err => {});
-    const data = await JSON.parse(res)
+    const data = JSON.parse(res)
+
+    console.log(data)
    
     return data
   }
